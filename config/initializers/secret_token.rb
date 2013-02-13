5a623ea429b0f107d18bb53dd4ae5a0f76e4aa6a
Jasmin Besic <jasmin@optyn.com>
2013-02-13 15:50:42 -0600

    2nd add

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = 'f000497b852e48bc86cfcdbe628bf0e8a0784ec982015aa626b89cc1e0f81571bc25aec055879717c3b998d2d1956823e763e450b6c9b79e58931db52f7584a5'
+DemoApp::Application.config.secret_token = 'f2c63481c0380424b244ce1b07ccc18164566879e32a68e1b5bc14c2a8df9da331224006e98a3b159438c127bdc435fe5cff2c1af4d741c5a669b066752503e5'
